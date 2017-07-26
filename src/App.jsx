618b38a7efd2248ee2363ea577fc1e61313edd14
Ayesha <ayesha_04@yahoo.com>
2017-07-26 19:19:54 +0000

    added websocket server, connected to it, sent a message to the server

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
+
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -20,49 +22,67 @@ class App extends Component {
       }
   }
 
-  handleKeyPress = (event) => {
-      console.log("handleKeyPress")
-      console.log(event.key)
-      if(event.key === "Enter") {
-        const newMessage = {
-        username: this.state.currentUser.name,
-        content: event.target.value
-      };
+  // handleKeyPress = (event) => {
+  //     console.log("handleKeyPress")
+  //     console.log(event.key)
+  //     if(event.key === "Enter") {
+  //       const newMessage = {
+  //         username: this.state.currentUser.name,
+  //         content: event.target.value
+  //       };
+  //       sendText(newMessage);
+  //     }
+  //     const messages = this.state.messages.concat(newMessage)
+  //     this.setState({messages: messages})
+  //   }
+  // }
 
-      const messages = this.state.messages.concat(newMessage)
-      this.setState({messages: messages})
-    }
+  sendMessage = (message) => {
+    // update local state with new message
+    this.setState((prevState) => {
+      return { messages: prevState.messages.concat(message) }
+    })
+
+    // send the message to our server
+    sendText(message);
   }
 
   // in App.jsx
-componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-  //   handleSubmit(e) {
-  //   e.preventDefault();
-  //   var newMessage =  {
-  //     id: i,
-  //     username: this.state.currentUser,
-  //     content: this.state.content
-  //   };
-  //   this.setState((prevState) => ({
-  //     messages: prevState.messages.concat(newMessages)
-  //   }));
-  // }
-    const newMessage = {id: 3, username: "Michelle", content: "Hello There"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-      //this.setState()  // change the state. this calls render() and the component updates. so only use when state changes
-    }, 3000)
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+
+    let socket = new WebSocket("ws://localhost:3001")
+
+    socket.onopen = function (event) {
+      ///Socket.send("Connected to server");
+      console.log("Connected to server");
+    };
+
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+
+      const newMessage = {id: 3, username: "Michelle", content: "Hello There"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+        //this.setState()  // change the state. this calls render() and the component updates. so only use when state changes
+      }, 3000)
+
+    this.socket = socket;
   }
 
+  sendText(message) {
+  // Construct a msg object containing the data the server needs to process the message from the chat client.
+    var msg = {
+      type: "sendMessage",
+      username: message.username,
+      content: message.content
+    };
 
-
-
+    // Send the msg object as a JSON-formatted string.
+    this.socket.send(JSON.stringify(msg));
+  }
 
   render() {
     console.log("Rendering <App/>")
@@ -72,7 +92,7 @@ componentDidMount() {
           <a className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={this.state.messages} />
-        <ChatBar currentUser={this.state.currentUser.name} handleKeyPress={this.handleKeyPress} />
+        <ChatBar currentUser={this.state.currentUser.name} sendMessage={this.sendMessage} />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,14 +2,40 @@ import React, {Component} from 'react';
 
 class ChatBar extends Component {
 
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: props.currentUser.name,
+      message: ''
+    }
+  }
+
+  handleMessageKeyPress = (event) => {
+    if(event.key === "Enter") {
+      const newMessage = {
+        username: this.state.username,
+        content: event.target.value
+      };
+      // send message to parent component to send to server
+      this.props.sendMessage(newMessage);
+      // reset text box to empty string
+      this.setState({message: ''});
+    } else {
+      // update state with current text box content
+      this.setState({message: event.target.value});
+    }
+  }
+
         //onChange={this.handleChange}
 //onKeyPress={this.handleKeyPress}
   render() {
     console.log("Rendering <App/>");
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={this.props.currentUser}/>
-        <input className="chatbar-message" type="" onKeyPress={this.props.handleKeyPress} />
+        <input className="chatbar-username" value={this.state.username} }/>
+        <input className="chatbar-message" type="text" value={this.state.message} onKeyPress={this.handleMessageKeyPress} />
       </footer>
     );
   }
